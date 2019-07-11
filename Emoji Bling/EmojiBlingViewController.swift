6f0b0e275977f6999c8c9cb62c58c055278e7be3
faiz mohideen <>
2019-07-11 12:46:46 +0100

    Implement ARSCNViewDelegate functions

--- Emoji Bling/EmojiBlingViewController.swift
+++ Emoji Bling/EmojiBlingViewController.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2019 faiz mohideen. All rights reserved.
 //
 
+// Following: https://www.raywenderlich.com/5491-ar-face-tracking-tutorial-for-ios-getting-started#toc-anchor-003
+
 import UIKit
 import ARKit
 
@@ -14,10 +16,10 @@ class EmojiBlingViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         guard ARFaceTrackingConfiguration.isSupported else {
             fatalError("Face tracking is not supported on this device")
         }
+        sceneView.delegate = self
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -29,9 +31,24 @@ class EmojiBlingViewController: UIViewController {
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-
         sceneView.session.pause()
     }
-
 }
 
+extension EmojiBlingViewController: ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        guard let device = sceneView.device else {  return nil }
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+        return node
+    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+            let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+                return
+        }
+        faceGeometry.update(from: faceAnchor.geometry)
+    }
+}
